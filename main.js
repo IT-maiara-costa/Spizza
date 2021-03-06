98a1bfc28d335c92820c81aacea87ba65fb33740
Maiara Costa <it.maiara.souza.br@gmail.com>
2021-03-06 18:16:28 -0300

    Comitando site estático

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menuBox = document.querySelector('.toggle-menu');
+    var menu = document.querySelector('.nav-ul')
+      menu.classList.toggle('show');
+      menuBox.classList.toggle('show');
+}
